308d32b7873da5878e3b7baee681893354721755
Keith Williams <keith.dev.uk@gmail.com>
2020-01-28 09:20:51 +0000

    use fts in instead of dirent, use unbuffered file reads, remove limit on md5 stamps

--- filedup.cc
+++ filedup.cc
@@ -25,19 +25,19 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <fcntl.h>
 #include <fts.h>
 
 #include <iostream>
 #include <iomanip>
-#include <fstream>
 #include <stdexcept>
 #include <memory>
 
 #include <string.h>
+#include <stdio.h>
 
 //---------------------------------------------------------------------------
 
-void scan_dir (files_t& files, options_t& opts, const std::string& dir);
 void scan_file(files_t& files, options_t& opts, file_info_t& file);
 
 #ifdef USE_FTS_CMP_CONST_PTR
@@ -59,11 +59,9 @@ try
 		if (std::regex_search(name, regex))
 			 throw std::runtime_error("excluding: " + name);
 
-//	std::unique_ptr<char, void(*)(void*)> namech(strdup(name.c_str()), free);
 	std::unique_ptr<char, decltype(&free)> namech(strdup(name.c_str()), free);
 	char* paths[] = { namech.get(), nullptr };
 
-//	std::unique_ptr<FTS, int(*)(FTS*)> ftsp(fts_open(paths, FTS_PHYSICAL|FTS_XDEV, mastercmp), fts_close);
 	std::unique_ptr<FTS, decltype(&fts_close)> ftsp(fts_open(paths, FTS_PHYSICAL|FTS_XDEV, mastercmp), fts_close);
 	if (!ftsp.get()) {
 		// 'name' is a one-off filename
@@ -82,10 +80,37 @@ try
 	}
 
 	FTSENT *ftsentp = fts_read(ftsp.get());
+	if (!ftsentp)
+		throw std::runtime_error("cannot read: " + name);
+
 	switch (ftsentp->fts_info) {
 	case FTS_D:		// directory
 		if (opts.verbose > DBG_LEVEL_1) dbg << "type directory: " << name << "\n";
-		scan_dir(files, opts, name);
+		if (FTSENT* ftschild = fts_children(ftsp.get(), 0)) {
+			std::string name;
+
+			do {
+				name.reserve(std::max(name.capacity(), ftsentp->fts_pathlen + 1 + ftschild->fts_namelen));
+				name  = ftsentp->fts_path;
+				name += "/";
+				name += ftschild->fts_name;
+
+				if (S_ISDIR(ftschild->fts_statp->st_mode)) {
+					scan(files, opts, name);
+				}
+				else {
+					file_info_t rec = make_fileinfo(
+						ftschild->fts_statp->st_ino,
+						ftschild->fts_statp->st_nlink,
+						ftschild->fts_statp->st_size,
+						std::move(name));
+					scan_file(files, opts, rec);
+				}
+
+				ftschild = ftschild->fts_link;
+			}
+			while (ftschild);
+		}
 		break;
 	case FTS_F:	{	// file
 		if (opts.verbose > DBG_LEVEL_1) dbg << "type file: " << name << "\n";
@@ -121,55 +146,40 @@ catch (const std::exception &e)
 	if (opts.verbose > DBG_LEVEL) dbg << e.what() << "\n";
 }
 
-// TODO: use fts stuff instead of dir stuff, integrate with scan(), and deprecate this function.
-void scan_dir(files_t& files, options_t& opts, const std::string& dirname)
-{
-	if (DIR* d = opendir(dirname.c_str())) {
-		while (struct dirent* entry = readdir(d)) {
-			if ((entry->d_name[0] == '.' && entry->d_name[1] == '\0') ||
-				(entry->d_name[0] == '.' && entry->d_name[1] == '.' && entry->d_name[2] == '\0'))
-				continue;
-
-			std::string name = dirname + std::string((dirname.back() != '/' ? 1 : 0), '/') + entry->d_name;
-			scan(files, opts, name);
-		}
-
-		closedir(d);
-	}
+namespace {
+	struct context_t {
+		static constexpr size_t datasz{64 * 1024};
+		unsigned char data[datasz];
+	};
+	context_t ctx;
 }
 
 void scan_file(files_t& files, options_t& opts, file_info_t& info)
 try
 {
-	std::string filename = file_name(info);
-	off_t       filesize = file_size(info);
+	const std::string filename = file_name(info);
 
-	const off_t K = 1024 * 1;
-	const off_t M = 1024 * K;
-	const off_t G = 1024 * M;
+	MD5_CTX md_context;
+	MD5_Init(&md_context);
 
-#ifdef BITS64
-	if (filesize > 4*G-1)           throw std::runtime_error("skip large file: " + filename);
-#endif
-#ifdef BITS32
-	if (filesize > 2*G-1)           throw std::runtime_error("skip large file: " + filename);
-#endif
-	if (filesize < opts.threshold)  throw std::runtime_error("threshold breach: " + filename);
-	if (opts.verbose > DBG_LEVEL_0) dbg << "filename: " << filename << "\n";
-
-	std::ifstream is(filename, std::ios::binary);
-	if (!is)                        throw std::runtime_error("cannot open: " + filename);
-
-	std::unique_ptr<char[]> buf(new char[filesize]);
-	is.read(buf.get(), filesize);
+	int fd;
+	if ((fd = open(filename.c_str(), O_RDONLY)) != -1) {
+		int nbytes;
+		while ((nbytes = read(fd, ctx.data, sizeof(ctx.data))) != 0)
+			MD5_Update(&md_context, ctx.data, nbytes);
+		close(fd);
+	}
+	else
+		throw std::runtime_error{"cannot open: " + filename};
 
 	md5_t sig;
-	MD5((unsigned char*)buf.get(), filesize, sig.value);
+	MD5_Final(sig.value, &md_context);
+
 	files.emplace(std::make_pair(sig, std::move(info)));
 }
 catch (const std::bad_alloc &)
 {
-	std::string filename = file_name(info);
+	const std::string filename = file_name(info);
 	if (opts.verbose > DBG_LEVEL) dbg << "file too large: " << filename << "\n";
 }
 catch (const std::exception &e)
@@ -179,39 +189,39 @@ catch (const std::exception &e)
 
 //---------------------------------------------------------------------------
 
-void show_duplicates(const files_t& files, options_t& opts, std::ostream& os);
+namespace {
+	void show_duplicates(const files_t& files, options_t& opts, std::ostream& os)
+	{
+		if (files.empty()) return;
+
+		auto p = files.begin();
+		auto* key = &p->first;
+		auto* val = &p->second;
+
+		for (;;) {
+			if (++p == files.end()) return;
+
+			if (p->first == *key) {
+				os << "\n";
+				os << static_cast<std::string>(file_name(*val)) << "\n";
+
+				do {
+					key = &p->first;
+					val = &p->second;
+					os << static_cast<std::string>(file_name(*val)) << "\n";
+
+					if (++p == files.end()) return;
+				}
+				while (p->first == *key);
+			}
+
+			key = &p->first;
+			val = &p->second;
+		}
+	}
+}
 
 void show(const files_t& files, options_t& opts)
 {
 	show_duplicates(files, opts, std::cout);
-}
-
-void show_duplicates(const files_t& files, options_t& opts, std::ostream& os)
-{
-	if (files.empty()) return;
-
-	auto p = files.begin();
-	auto* key = &p->first;
-	auto* val = &p->second;
-
-	for (;;) {
-		if (++p == files.end()) return;
-
-		if (p->first == *key) {
-			os << "\n";
-			os << static_cast<std::string>(file_name(*val)) << "\n";
-
-			do {
-				key = &p->first;
-				val = &p->second;
-				os << static_cast<std::string>(file_name(*val)) << "\n";
-
-				if (++p == files.end()) return;
-			}
-			while (p->first == *key);
-		}
-
-		key = &p->first;
-		val = &p->second;
-	}
 }
